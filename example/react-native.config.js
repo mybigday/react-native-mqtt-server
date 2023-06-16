ac08d89bfba9d45a08aecb0e2c32cf43034bc099
Hans <admin@itsrv.tw>
2023-06-16 14:20:26 +0800

    chore: fix lint error

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,13 +1,19 @@
 const pak = require('../package.json');
 
-const modules = [...Object.keys(pak.dependencies), ...Object.keys(pak.peerDependencies)]
-  .filter((name) => name.startsWith('react-native-'));
+const modules = [
+  ...Object.keys(pak.dependencies),
+  ...Object.keys(pak.peerDependencies),
+].filter((name) => name.startsWith('react-native-'));
 
 module.exports = {
   dependencies: Object.fromEntries(
     modules.map((name) => [
       name,
-      { root: require.resolve(name + '/package.json').replace('/package.json', '') }
+      {
+        root: require
+          .resolve(`${name}/package.json`)
+          .replace('/package.json', ''),
+      },
     ])
   ),
 };
